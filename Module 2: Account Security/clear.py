3b7fd59db206e5307dae297c98f2a43f68567101
Daniel Choo <czhdaniel@gmail.com>
2024-07-22 00:40:49 +0800

    Updated role deletion

--- Module 2: Account Security/clear.py
+++ Module 2: Account Security/clear.py
@@ -3,6 +3,7 @@ from botocore.exceptions import ClientError
 
 s3 = boto3.resource('s3')
 s3_client = boto3.client('s3')
+iam = boto3.client('iam')
 
 def clear_bucket(bucket_name):
 	print(f"Deleting bucket {bucket_name}")
@@ -34,10 +35,61 @@ def get_bucket_names():
 
 	return buckets
 
+def delete_demo_iam_entities():
+	prefix="demo"
+
+	# Delete IAM Users
+	try:
+		users = iam.list_users()['Users']
+		for user in users:
+			if user['UserName'].startswith(prefix):
+				print(f"Deleting user:{user['UserName']}")
+				# Additional safety check: Check if user has active access keys
+				keys = iam.list_access_keys(UserName=user['UserName'])['AccessKeyMetadata']
+				for key in keys:
+					iam.delete_access_key(UserName=user['UserName'], AccessKeyId=key['AccessKeyId'])
+				iam.delete_user(UserName=user['UserName'])
+				print(f"Deleted user: {user['UserName']}")
+	except ClientError as e:
+		print(f"Error listing or deleting users: {user['UserName']}")
+		print(e)
+		exit(0)
+
+	# Delete IAM Roles
+	try:
+		roles = iam.list_roles()['Roles']
+		for role in roles:
+			if role['RoleName'].startswith(prefix):
+				print(f"Deleting role:{role['RoleName']}")
+				# Detach all policies before deleting
+				for policy in iam.list_attached_role_policies(RoleName=role['RoleName'])['AttachedPolicies']:
+					iam.detach_role_policy(RoleName=role['RoleName'], PolicyArn=policy['PolicyArn'])
+				iam.delete_role(RoleName=role['RoleName'])
+				print(f"Deleted role: {role['RoleName']}")
+	except ClientError as e:
+		print(f"Error listing or deleting roles: {role['roleName']}")
+		print(e)
+		exit(0)
+	
+	# Delete IAM Policies
+	try:
+		policies = iam.list_policies(Scope='Local')['Policies'] 
+		for policy in policies:
+			if policy['PolicyName'].startswith(prefix):
+				print(f"Deleting policy: {policy}" )
+				# Ensure policy is not the default version before deleting
+				if not policy['IsDefaultVersion']:
+					iam.delete_policy(PolicyArn=policy['Arn'])
+	except ClientError as e:
+		print(f"Error listing or deleting policy: {policy['Arn']}")
+		print(e)
+		exit(0)
+
 def main():
-	bucket_names = get_bucket_names()
-	for bucket in bucket_names:
-		clear_bucket(bucket)
+	# bucket_names = get_bucket_names()
+	# for bucket in bucket_names:
+	# 	clear_bucket(bucket)
+	delete_demo_iam_entities() 
 
 if __name__ == '__main__':
 	main()
