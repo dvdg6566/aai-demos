9e3617c4c6177f11e383415a25c992dfabb989c6
dvdg6566 <czhdaniel@gmail.com>
2024-08-07 19:25:22 +0800

    some stuff for cloudformation

---
+++ Module 8 - Automation/deploy.sh
@@ -0,0 +1,4 @@
+aws cloudformation deploy \
+--template-file template.yml \
+--stack-name "demo-070824" \
+--capabilities CAPABILITY_NAMED_IAM
---
+++ Module 8 - Automation/fail.sh
@@ -0,0 +1,4 @@
+aws cloudformation deploy \
+--template-file fail.yml \
+--stack-name "demo-07082401" \
+--capabilities CAPABILITY_NAMED_IAM
