ba636ce644e1a6718fce8430ffb397a6f22bc5f2
dvdg6566 <czhdaniel@gmail.com>
2024-08-31 21:48:01 +0800

    Main creation template for networking 2

---
+++ Module 10 - Networking 2/clear.py
@@ -0,0 +1,119 @@
+import boto3
+import time
+
+ec2 = boto3.client('ec2')
+ec2_resource = boto3.resource('ec2')   
+
+def list_vpcs():
+    response = ec2.describe_vpcs()
+    vpcs = response.get('Vpcs', [])
+    ids = []
+    for vpc in vpcs:
+        vpcname = ""
+        for tag in vpc.get('Tags', []):
+            if tag['Key'] == 'Name':
+                vpcname = tag['Value']
+        if "demo" not in vpcname: continue
+        ids.append(vpc['VpcId'])
+        print(f"VPC ID: {vpc['VpcId']}, CIDR: {vpc['CidrBlock']}")
+    return ids
+
+def list_instances():
+    response = ec2.describe_instances()
+    reservations = response['Reservations']
+    instance_ids = []
+    for reservation in reservations:
+        for instance in reservation['Instances']:
+            instance_id = instance['InstanceId']
+            instance_obj = ec2_resource.Instance(instance_id)
+            instance_name = ""
+
+            for tag in instance.get('Tags', []):
+                if tag['Key'] == 'Name':  # Adjust if you use a different tag key
+                    instance_name = tag['Value']
+
+            if "demo" not in instance_name: continue
+            if instance["State"]["Name"] == "terminated": continue
+
+            instance_ids.append(instance_id)
+            
+    return instance_ids
+
+# 2. Terminate instances
+def terminate_instances(instance_ids):
+    if instance_ids:
+        ec2.terminate_instances(InstanceIds=instance_ids)
+        print("The following instances have been terminated:")
+        for instance_id in instance_ids:
+            print(instance_id)
+        time.sleep(20)
+    else:
+        print("No running instances found to terminate.")
+
+def delete_subnets(vpc_id):
+    vpc = ec2_resource.Vpc(vpc_id)
+
+    # Delete dependencies
+    for subnet in vpc.subnets.all():
+        for interface in subnet.network_interfaces.all():
+            interface.delete()
+        subnet.delete()
+
+    for gateway in vpc.internet_gateways.all():
+        vpc.detach_internet_gateway(InternetGatewayId=gateway.id)
+        gateway.delete()
+
+    filters = [{'Name': 'vpc-id', 'Values': [vpc.id]}]
+    response = ec2.describe_vpc_endpoints(Filters=filters)
+    endpoint_ids = [endpoint['VpcEndpointId'] for endpoint in response['VpcEndpoints'] 
+                    if endpoint['VpcEndpointType'] == 'Gateway']
+
+    if endpoint_ids:
+        ec2.delete_vpc_endpoints(VpcEndpointIds=endpoint_ids)
+        print("The following gateway endpoints have been deleted:")
+        for endpoint_id in endpoint_ids:
+            print(endpoint_id)
+
+def delete_vpc(vpc_id):
+    print()
+    print("Deleting vpc", vpc_id)
+    vpc = ec2_resource.Vpc(vpc_id)
+
+    for route_table in vpc.route_tables.all():
+        print(route_table)
+        for association in route_table.associations:
+            print(association, association.main)
+            if not association.main:
+                association.delete()
+
+        # Delete the routes in the route table
+        for route in route_table.routes:
+            if route.destination_cidr_block != vpc.cidr_block:
+                ec2.delete_route(  # Use the client to delete the route
+                    RouteTableId=route_table.id,
+                    DestinationCidrBlock=route.destination_cidr_block
+                )
+
+        try: 
+            route_table.delete()
+        except Exception as e:
+            print(e)
+
+    for security_group in vpc.security_groups.all():
+        if security_group.group_name != 'default':
+            security_group.delete()
+
+    # # Delete VPC itself
+    vpc.delete()
+    print(f"VPC ID: {vpc_id} deleted")
+
+def main():
+    vpcs = list_vpcs()
+
+    for vpc in vpcs:
+        delete_subnets(vpc)
+    for vpc in vpcs:
+        delete_vpc(vpc)
+
+if __name__ == '__main__':
+    main()
---
+++ Module 10 - Networking 2/setup.py
@@ -0,0 +1,168 @@
+import boto3
+from botocore.exceptions import ClientError
+from secrets import token_hex
+import time
+import json
+
+ec2 = boto3.resource('ec2')
+ec2_client = boto3.client('ec2')
+iam = boto3.client('iam')
+lambda_client = boto3.client('lambda')
+
+AZ = 'ap-southeast-1a'
+subnetID = ""
+securityGroupId = ""
+
+def launch_subnet(vpcid, cidr):
+	global subnetID
+
+	subnet = ec2.create_subnet(
+		CidrBlock=cidr, 
+		VpcId=vpcid, 
+		AvailabilityZone=AZ
+	)  
+	ec2_client.modify_subnet_attribute(
+		SubnetId=subnet.id,
+		MapPublicIpOnLaunch={'Value': True}
+	)
+	
+	print()
+	print("Subnet created: ", cidr, subnet.id)
+
+	response = ec2_client.create_route_table(VpcId=vpcid)
+	route_table_id = response['RouteTable']['RouteTableId']
+	ec2_client.associate_route_table(
+		RouteTableId=route_table_id,
+		SubnetId=subnet.id
+	)
+	print("Created route table ", route_table_id)
+
+	subnetID = subnet.id
+
+	return subnet
+
+def create_security_group_in_vpc(vpc_id, group_name):
+    ec2_client = boto3.client('ec2')
+
+    try:
+        response = ec2_client.create_security_group(
+            GroupName=group_name,
+            Description="placeholder",
+            VpcId=vpc_id
+        )
+
+        global securityGroupId
+        securityGroupId = response['GroupId']
+        print(f"Security group '{group_name}' created with ID: {securityGroupId}")
+        return securityGroupId
+
+    except Exception as e:
+        print(f"Error creating security group!")
+        print(e)
+        exit(0)
+
+        return None
+
+def setup_vpc(prefix):
+	vpc_cidr_block = '10.0.0.0/16' 
+	subnet_cidr = '10.0.1.0/24'
+
+	# Create VPC
+	vpc = ec2.create_vpc(CidrBlock=vpc_cidr_block)
+	vpc.create_tags(Tags=[{'Key': 'Name', 'Value': f'{prefix}-vpc'}])
+	vpc.wait_until_available()
+	print(f"Created VPC name {prefix}-vpc!")
+
+	# Launch 1 private Subnet
+	launch_subnet(vpc.id, subnet_cidr)
+
+	create_security_group_in_vpc(vpc.id, f"{prefix}-sg")
+
+def create_lambda_iam_role(prefix):
+	role_name = f"{prefix}-lambda-role"
+
+	trust_policy = {
+		"Version": "2012-10-17",
+		"Statement": [
+			{
+				"Effect": "Allow",
+				"Principal": {
+					"Service": "lambda.amazonaws.com"
+				},
+				"Action": "sts:AssumeRole"
+			}
+		]
+	}
+
+	try: 
+		response = iam.create_role(
+			RoleName = role_name,
+			AssumeRolePolicyDocument=json.dumps(trust_policy)
+		)
+
+		role_arn = response['Role']['Arn']
+		print(f"Created Lambda IAM Role with arn: {role_arn}")
+
+		iam.attach_role_policy(
+			RoleName = role_name,
+			PolicyArn='arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole'
+		)
+
+		print(f"Attached basic execution role to {role_arn}")
+		
+		time.sleep(10)
+
+	except ClientError as e:
+		print("Error creating Lambda IAM role!")
+		print(e)
+		exit(0)
+
+	return role_arn
+
+def create_lambda_function(prefix, subnet_ids, sg_ids):
+	function_name = f"{prefix}-listing"
+	code_file = "lambda.zip"
+
+	with open(code_file, "rb") as f:
+		zip_file = f.read()
+
+	role_arn = create_lambda_iam_role(prefix)
+	
+	try:
+		response = lambda_client.create_function(
+			FunctionName=function_name,
+			Runtime='python3.11',
+			Role=role_arn,
+			Code={'ZipFile': zip_file},
+			Handler='lambda_function.lambda_handler',
+			Timeout=30,  # Adjust timeout as needed
+			MemorySize=128,  # Adjust memory as needed
+			Publish=True,  # Publish the function immediately
+			VpcConfig={
+                'SubnetIds': subnet_ids,
+                'SecurityGroupIds': sg_ids
+            }
+		)
+
+		print("Created Lambda function!")
+
+	except ClientError as e:
+		print(f"Error creating lambda function")
+		print(e)
+		exit(0)
+
+	return role_arn
+
+def main():
+	prefix = "demo" + token_hex(4)
+	setup_vpc(prefix)
+
+	global subnetID, securityGroupId
+
+	print("Created subnetID ", subnetID)
+	print("Created SG", securityGroupId)
+
+	lambda_role_arn = create_lambda_function(prefix, [subnetID], [securityGroupId])
+
+if __name__ == '__main__':
+	main()
