ef369cd8b6b9710038991bcb7749c481e004b32e
dvdg6566 <czhdaniel@gmail.com>
2024-09-06 19:17:27 +0800

    Removd SH files for templates

--- Module 8 - Automation/deploy.sh
+++
@@ -1,4 +0,0 @@
-aws cloudformation deploy \
---template-file template.yml \
---stack-name "demo-070824" \
---capabilities CAPABILITY_NAMED_IAM
--- Module 8 - Automation/fail.sh
+++
@@ -1,4 +0,0 @@
-aws cloudformation deploy \
---template-file fail.yml \
---stack-name "demo-070824-fail" \
---capabilities CAPABILITY_NAMED_IAM
